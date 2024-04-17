ec2e8b56383eb1c873311f998ce008f7e7784caf
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-04-17 14:06:30 -0400

    avance 17/04/2024

--- CosasRut.py
+++ CosasRut.py
@@ -1,18 +1,20 @@
 import random as rn
+import math
 
 class CosasRut:
     def __init__(self):
         pass
     
     def validaRut(rut):#10.200.300-6
+        
         try:
             rutschar = ''
-            caracteres="0123456789k"
+            caracteres="0123456789K"
             for x in rut:
                 if x in caracteres:
                     rutschar += x
-                elif x == 'K':
-                    rutschar += 'k'
+                elif x == 'k':
+                    rutschar += 'K'
 
             rutsindigito=rutschar[:len(rutschar)-1]#10200300
             rutinvertido=rutsindigito[::-1]#00300201
@@ -38,10 +40,39 @@ class CosasRut:
         except:
             return False
 
-    def creaRut(self):
+    def creaRut(self, g1=None,g2=None,g3=None,div=None):
+        casos = [g1,g2,g3,div]
+        bin = 0
+        for i in range(len(casos)):
+            if casos[i] != None:
+                bin += int(math.pow(2,i))
+        
         rutDefinitivo = ''
         while True:
-            rutInt = rn.randint(8000000,21000000)
+            if bin == 0 or bin == 8:    #0000  0001
+                rutInt = rn.randint(8000000,21000000)
+                
+            elif bin == 1 or bin == 9:  #1000  1001
+                rutInt = rn.randint(g1*1000000,(g1+1)*1000000)
+                
+            elif bin == 2 or bin == 10: #0100  0101
+                rutInt = rn.randint(8,21)*1000000+((g2*1000)+rn.randint(0,1000))# (8000000 - 21000000) + 600000 + (0-1000)
+                
+            elif bin == 3 or bin == 11: #1100  1101
+                rutInt = rn.randint((g1*1000000)+(g2*1000),((g1)*1000000)+((g2+1)*1000))#20.600.000 - 20.700.000
+                
+            elif bin == 4 or bin == 12: #0010  0011
+                rutInt = (rn.randint(8000,21000)*1000)+g3
+                
+            elif bin == 5 or bin == 13: #1010  1011
+                rutInt = (rn.randint((g1*1000),((g1+1)*1000))*1000)+(g3)#[20.000.000-21.000.000]
+                
+            elif bin == 6 or bin == 14: #0110  0111
+                rutInt = rn.randint(8,21)*1000000+((g2*1000)+g3)
+            
+            elif bin == 7 or bin == 15: #1110: #1111
+                rutInt = (g1*1000000)+(g2*1000)+g3
+            
             rutStr = str(rutInt)
             #9312438
             #8342139
@@ -55,25 +86,47 @@ class CosasRut:
                     rutPts = rutPts + rutStri[i]
             
             rutPtsi = rutPts[::-1]
-            rut = rutPtsi+"-"+rn.choice(["0","1","2","3","4","5","6","7","8","9","k"])
-        
+            digver = ''
+            if bin >= 8:
+                if div == 'k':
+                    div = 'K'
+                digver = str(div)
+            else:
+                digver = rn.choice(["0","1","2","3","4","5","6","7","8","9","K"])
+            rut = rutPtsi+"-"+digver
+            
             if self.validaRut(rut):
                 rutDefinitivo = rut
                 break
         return rutDefinitivo
 
+    def corrigeRut(self,rut,similitud):
+        texto1 = rut
+        while True:
+            texto2 = self.creaRut(self,g1=int(texto1[:2]))
+            exactitud = 0
+            cont = 0
+            for i in range(min(len(texto1),len(texto2))):
+                if(texto1[i] == texto2[i]):
+                    cont += 1
+            exactitud = int(round((cont/max(len(texto1),len(texto2)))*100,0))
+            if exactitud >= similitud:
+                break
+                
+        return texto2
+    
     def limpiaRut(rut):
         newRut = ''
         try:
             #print(rut)
             
-            caracteres="0123456789k"
+            caracteres="0123456789K"
             rutschar = ''
             for x in rut:
                 if x in caracteres:
                     rutschar += x
-                elif x=='K':
-                    rutschar += 'k'
+                elif x=='k':
+                    rutschar += 'K'
             #print(rutschar)
             
             dv = rutschar[len(rutschar)-1] #     8
--- filtro.py
+++ filtro.py
@@ -1,30 +1,162 @@
 import pandas as pd
 import os
+import math
+from CosasRut import CosasRut
 
 def dfList(list):
     fList = []
     for i in list:
-        fList.append(pd.read_excel('Files/'+i))
+        if i.endswith(".xlsx"):
+            fList.append(pd.read_excel('Files/'+i))
     return fList
 
+def traducir(txt):
+    dictio = {
+        'dmt2':'Diabetes tipo 2',
+        'hta,dm2':'Hipertensión y diabetes tipo 2',
+        'dm, hta':'Hipertensión y diabetes',
+        'dm,hta,dlp':'Hipertensión, diabetes y dislipidemia',
+        'No aplica':'No aplica',
+        'hta ':'Hipertensión',
+        'no':'No aplica',
+        'ca,dm':'Cáncer',
+        'hta, resistencia a la insulina':'Hipertensión y resistencia a la insulina',
+        'dm':'Diabetes',
+        'ca':'Cáncer',
+        'dm2,ca':'Diabetes tipo 2 y cáncer',
+        'hta':'Hipertensión',
+        'resistencia a la insulina':'Resistencia a la insulina',
+        'hta,dm':'Hipertensión y diabetes',
+        'dm,hta':'Hipertensión y diabetes',
+        'hermana, mama hipotiroidismo y abuela DM':'Hipotiroidismo y diabetes',
+        'madre, padre HTA y DMT2':'Hipertensión y diabetes tipo 2',
+        'No':'No aplica',
+        'abuela DM':'Diabetes',
+        'abuelo DM':'Diabetes',
+        'mama hipotiroidismo y abuela HTA':'Hipotiroidismo e hipertensión',
+        'abuelo HTA':'Hipertensión',
+        'HTA DM':'Hipertensión y diabetes',
+        'padre,madre HTA':'Hipertensión',
+        'mama DM abuelo DM':'Diabetes',
+        'diabetes, displemia ':'Diabetes y displemia',
+        'hpertension':'Hipertensión',
+        'hipertension,dislipidemia':'Hipertensión y displemia',
+        'hipertension,diabetes':'Hipertensión y diabetes',
+        'hipertencsion':'Hipertensión',
+        'hipertension':'Hipertensión',
+        'Hipertensión Padre y madre':'Hipertensión',
+        ' Abuelo Diabetes':'Diabetes',
+        'paterna, hipertension':'Hipertensión',
+        'Hipertension Abuelo':'Hipertensión',
+        'papá Diabetes, Hipertension':'Hipertensión y diabetes',
+        'abuelos, Diabetes, Hipertension':'Hipertensión y diabetes',
+        'mamá, Hipertension':'Hipertensión',
+        'mamá Diabetes, papá Hipertension':'Hipertensión y diabetes',
+        'padre diabetes, madre hipertension y cancer':'Hipertensión, diabetes y cáncer',
+        'papá Hipertension, abuelo paterno Diabetes':'Hipertensión y diabetes',
+        'dm,hta,acu':'Hipertensión, diabetes e ACCU',
+        'hma(+) dm(-)':'Diabetes y hematuria',
+        'dm,tha,ob':'Diabetes, hiperactividad y obesidad',
+        'hipotiroidismo,dm,tha':'Diabetes, hiperactividad e Hipotiroidismo',
+        'hta,dm,dislipidemia':'Hipertensión, diabetes y Dislipidemia',
+        'atc,cuc,trigliceridos':'Antidepresivos, colitis ulcerosa y Trigliceridos',
+        'res insulina':'Resistencia a la insulina',
+        'Abuela HTA':'Hipertensión',
+        'Madre HTA':'Hipertensión',
+        'Madre y Padre DMT2':'Diabetes tipo 2',
+        'Abuela Diabetes':'Diabetes',
+        'HTA padres, DMT2 Madre':'Hipertensión y diabetes tipo 2',
+        'DM padre, madre HTA':'Hipertensión y diabetes',
+        'Padre DM':'Diabetes',
+        'HTA':'Hipertensión',
+        'Hija DM, Abuela DM':'Diabetes',
+        'Mama DM':'Diabetes',
+        'hermano HTA':'Hipertensión',
+        'HTA, DM abuelo ':'Hipertensión y diabetes',
+        'papa y mama HTA':'Hipertensión',
+        ' HTA/DM':'Hipertensión y diabetes',
+        'Abuela HTA, Papa HTA':'Hipertensión',
+        'Mama= Resistencia a la insulinda- HTA, Abuela= DM':'Hipertensión, diabetes y resistencia a la insulina',
+        'Hipertiroidismo':'Hipertiroidismo',
+        'Abuelos= HTA Y DM':'Hipertensión y diabetes',
+        'MAMA= DM/Sarcoidosis':'Diabetes y Sarcoidosis',
+        'Abuelos y mama= HTA Y DM':'Hipertensión y diabetes',
+        'PADRES= HTA Y DM':'Hipertensión y diabetes',
+        'Abuela DM':'Diabetes',
+        'PAPA= DM2-HTA':'Hipertensión y diabetes tipo 2',
+        'DM':'Diabetes',
+        'HTA,DM':'Hipertensión y diabetes',
+        ' HTA,DM POEPO':'Hipertensión y diabetes',
+        'Abuelo HTA, Abuela DM2':'Hipertensión y diabetes tipo 2',
+        'HTA Papá':'Hipertensión',
+        'Papá HTA':'Hipertensión',
+        '(Abuelo - Madre) Cancer':'Cáncer'
+    }
+    return dictio[txt]
+
+def SiNo(op):
+    ops = {'si':'Si','no':'No',
+           'Si':'Si','No':'No'}
+    return ops[op]
 
 files = os.listdir('Files')
 
-dataframes = dfList(files)
+
+dataFrames = dfList(files)
 
 ruts = []
 
-for i in range(len(files)):
-    print('Archivo:',files[i])
-    print(dataframes[i])
-    print(dataframes[i].head())
-    #if 'Rut' in dataframes[i].head():
-    #    ruts.insert(dataframes[i]['Ruts'])
+columnas = dataFrames[0].columns
+
+datosDF = {}
+for i in range(len(columnas)):
+    datosDF.update({columnas[i]:[]})
+
+
+for i in range(len(dataFrames)):
+    for j in range(len(dataFrames[i]['Rut'])):
+        limpio = CosasRut.limpiaRut(dataFrames[i]['Rut'][j])
+        if not limpio in ruts and CosasRut.validaRut(limpio):
+            for k in columnas:
+                if k != 'Rut':
+                    if (k != 'Mujer >= 88' and k != 'hombre >=102'):
+                        if k == 'Antecedentes familiar':
+                            if isinstance(dataFrames[i][k][j], float) and math.isnan(dataFrames[i][k][j]):
+                                datosDF[k].append('No aplica')
+                            else:
+                                datosDF[k].append(traducir(dataFrames[i][k][j]))
+                        else:
+                            if isinstance(dataFrames[i][k][j], float) and math.isnan(dataFrames[i][k][j]):
+                                datosDF[k].append('No aplica')
+                            else:
+                                datosDF[k].append(dataFrames[i][k][j])
+                    else:
+                        if isinstance(dataFrames[i][k][j], float) and math.isnan(dataFrames[i][k][j]):
+                            datosDF[k].append('No aplica')
+                        else:
+                            datosDF[k].append(SiNo(dataFrames[i][k][j]))
+                else:
+                    datosDF[k].append(limpio)
+        elif not CosasRut.validaRut(limpio):
+            for c in dataFrames[i].columns:
+                print(dataFrames[i]['Rut'][j])
+                print(dataFrames[i][c][j],'\n')
+
+#for i in datosDF:
+#    print(i)
+    #for j in datosDF[i]:
+        #print(j)
+    """
+    if k == 'Antecedentes familiar':
     cont = input('continuar s/n: ')
     if cont != 's':
         break
     else:
         print('\n')
+    """
 print('\nFIN\n')
+DF = pd.DataFrame(datosDF,columns = columnas)
 
-print(ruts)
+#print(DF)
+#DF.to_excel('dataFrame.xlsx')
+
